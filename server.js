7f5244777347549b7eb1423cf0327d34aa3e2ff7
resoner0796 <ulisees.luna96@gmail.com>
2025-08-06 09:39:04 -0500

    Update server.js

--- server.js
+++ server.js
@@ -1,82 +1,49 @@
-const express = require('express');
-const http = require('http');
-const cors = require('cors');
-const { Server } = require('socket.io');
+const express = require("express");
+const http = require("http");
+const { Server } = require("socket.io");
+const cors = require("cors");
 
 const app = express();
-const server = http.createServer(app);
+app.use(cors({ origin: "https://resoner0796.github.io" })); // <-- PERMITE conexiones desde GitHub Pages
 
-// Configura CORS para aceptar tu dominio de GitHub Pages
+const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: 'https://resoner0796.github.io', // <--- PON TU DOMINIO GITHUB AQUÍ
-    methods: ['GET', 'POST']
+    origin: "https://resoner0796.github.io", // <-- PERMITE frontend externo
+    methods: ["GET", "POST"]
   }
 });
 
-const jugadores = {};
-let hostId = null;
-let historialCartas = [];
-let cartasSeleccionadas = new Set();
+app.get("/", (req, res) => {
+  res.send("Servidor de Lotería funcionando 🎉");
+});
 
-io.on('connection', (socket) => {
-  console.log(`Jugador conectado: ${socket.id}`);
+let cartasSeleccionadas = [];
 
-  socket.on('registrar-nickname', (nickname) => {
-    if (!hostId) hostId = socket.id;
+io.on("connection", (socket) => {
+  console.log("Jugador conectado");
 
-    jugadores[socket.id] = {
-      nickname,
-      host: socket.id === hostId
-    };
-
-    socket.emit('rol-asignado', jugadores[socket.id]);
-    io.emit('jugadores-actualizados', jugadores);
-    socket.emit('cartas-desactivadas', Array.from(cartasSeleccionadas));
-    socket.emit('historial-actualizado', historialCartas);
+  socket.on("registrarJugador", (nickname) => {
+    socket.nickname = nickname;
+    console.log(`Jugador registrado: ${nickname}`);
+    socket.emit("registrado", nickname);
   });
 
-  socket.on('seleccionar-carta', (carta) => {
-    cartasSeleccionadas.add(carta);
-    io.emit('cartas-desactivadas', Array.from(cartasSeleccionadas));
+  socket.on("seleccionarCarta", (carta) => {
+    cartasSeleccionadas.push(carta);
+    io.emit("actualizarCartasSeleccionadas", cartasSeleccionadas);
   });
 
-  socket.on('barajear', () => {
-    historialCartas = [];
-    io.emit('barajear');
+  socket.on("limpiarCartas", () => {
+    cartasSeleccionadas = [];
+    io.emit("actualizarCartasSeleccionadas", cartasSeleccionadas);
   });
 
-  socket.on('cantar-carta', (carta) => {
-    historialCartas.push(carta);
-    io.emit('carta-cantada', carta);
-  });
-
-  socket.on('detener-juego', () => {
-    io.emit('juego-detenido');
-  });
-
-  socket.on('loteria', (nickname) => {
-    io.emit('loteria-anunciada', nickname);
-  });
-
-  socket.on('disconnect', () => {
-    console.log(`Jugador desconectado: ${socket.id}`);
-    delete jugadores[socket.id];
-
-    if (socket.id === hostId) {
-      hostId = Object.keys(jugadores)[0] || null;
-      if (hostId) jugadores[hostId].host = true;
-    }
-
-    io.emit('jugadores-actualizados', jugadores);
+  socket.on("disconnect", () => {
+    console.log("Jugador desconectado");
   });
 });
 
-app.get('/', (req, res) => {
-  res.send('Servidor de Lotería funcionando 🎉');
-});
-
-const PORT = process.env.PORT || 3000;
-server.listen(PORT, () => {
-  console.log(`Servidor corriendo en el puerto ${PORT}`);
+server.listen(3000, () => {
+  console.log("Servidor escuchando en el puerto 3000");
 });
