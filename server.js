442cc46d24faf5b61771e7f6ac9952e2fedefdcc
resoner0796 <ulisees.luna96@gmail.com>
2025-08-11 05:54:23 -0500

    Actualizar server.js

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
 const admin = require('firebase-admin');
-const serviceAccount = require('./serviceAccountKey.json');
+const serviceAccount = JSON.parse(process.env.FIREBASE_ADMIN_CREDENTIALS);
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount)
 });
+
 const db = admin.firestore();
 const express = require('express');
 const http = require('http');
